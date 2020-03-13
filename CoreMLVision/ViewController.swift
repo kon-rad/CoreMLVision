95890e0a7b062d98c02ab7d78f71375374277268
konradgnat <konradmgnat@gmail.com>
2020-03-12 23:42:45 -0500

    init

--- CoreMLVision/ViewController.swift
+++ CoreMLVision/ViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    private var imagePicker = UIImagePickerController()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
